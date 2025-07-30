2a56934bc5c2f135ea5b51780ba9482b9d210f4e
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2025-07-30 11:00:09 +0530

    Added LandChainPage and route to App component

---
+++ src/types/LandTypes.ts
@@ -0,0 +1,35 @@
+export interface LandOwner {
+  id: number;
+  ownerId: string;
+  firstName: string;
+  lastName: string;
+  nic: string;
+  address?: string;
+  contactNo?: string;
+}
+
+export interface LandTransfer {
+  id: number;
+  transferDate: string;
+  verifiedBy: string;
+  blockIndex: number;
+  blockHash: string;
+  prevBlockHash: string;
+  fromLandOwner: LandOwner;
+  toLandOwner: LandOwner;
+}
+
+export interface LandDetails {
+  id: number;
+  landId: string;
+  landName: string;
+  landPlace: string;
+  landLat: number;
+  landLang: number;
+  landSize: number;
+  landValue: number;
+  landType: string;
+  registerDate: string;
+  landStatus: string;
+  priority: number;
+}
